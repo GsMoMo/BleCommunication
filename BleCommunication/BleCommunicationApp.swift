c50cd7ada8ef68ee0fbd69f720fd36e46cda6d59
Gary Wu (008689) <gary008689.wu@shl-medical.com>
2022-01-04 14:07:38 +0800

    Initial Commit

---
+++ BleCommunication/BleCommunicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BleCommunicationApp.swift
+//  BleCommunication
+//
+//  Created by Gary Wu (008689) on 2022/1/4.
+//
+
+import SwiftUI
+
+@main
+struct BleCommunicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BleCommunication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BleCommunication
+//
+//  Created by Gary Wu (008689) on 2022/1/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
